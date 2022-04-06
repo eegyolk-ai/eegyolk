2ce61d6712d36d01bd7c12b1f82ff873b5b857c5
drcandacemakedamoore <doctormakeda@gmail.com>
2022-04-06 15:58:03 +0200

    add new file structure

--- ePodium GitHub/EEG_explorer-master/config_template.py
+++
@@ -1,7 +0,0 @@
-# config.py template
-# Add your respective folder names and save as config.py
-
-ROOT =  "..."
-PATH_CODE =  "..."
-PATH_DATA =  "..."
-PATH_OUTPUT =  "..."
---
+++ ePodium_2021/EEG_age_prediction/config_template.py
@@ -0,0 +1,30 @@
+# config.py template
+# Add your respective folder names and save as config.py
+# Please note that there are two roots used here, these can be the same root
+
+# Root folder (EEG_age_prediction/)
+ROOT = ""
+
+# Second root for large data storage (e.g. External HDD)
+SECOND_ROOT = ""
+
+# Saved models
+PATH_MODELS = ROOT + "trained_models/"
+
+# Processed data for DL models
+PATH_DATA_PROCESSED_DL = ROOT + "Data/data_processed_DL/"
+
+# Processed data for DL models (reduced)
+PATH_DATA_PROCESSED_DL_REDUCED = ROOT + "Data/data_processed_DL_reduced/"
+
+# Processed data for ML models
+PATH_DATA_PROCESSED_ML = ROOT + "Data/data_processed_ML/"
+
+# EEG metadata folder
+PATH_METADATA = ROOT + "Data/ePODIUM_metadata/"
+
+# Raw EEG data folder
+PATH_RAW_DATA = SECOND_ROOT + "Data/"
+
+# Already preprocessed data for initial experiments - before using own preprocessing pipeline
+PATH_DATA_PROCESSED_OLD = SECOND_ROOT + "Preprocessed_old/Data/data_processed_old/"
