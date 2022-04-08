ed11d079739d76b31ec4afe0b972d44bdfa7193a
drcandacemakedamoore <doctormakeda@gmail.com>
2022-04-08 13:27:14 +0200

    update helper_functions

--- eegyolk/tests/test.py
+++ eegyolk/tests/test.py
@@ -6,6 +6,7 @@ import sys
 sys.path.insert(0, os.getcwd())
 
 from eegyolk.display_helper import make_ordinal
+from eegyolk.helper_functions import band_pass_filter
 
 
 class TestDisplayHelperMethods(unittest.TestCase):
@@ -14,5 +15,17 @@ class TestDisplayHelperMethods(unittest.TestCase):
         self.assertEqual(make_ordinal(5), '5th')
 
 
+# class TestFiltering(unittest.TestCase):
+
+#     def test_band_pass_filter(self):
+#         # need to hash for contents then assert equal
+#         self.assertEqual(band_pass_filter(sample_EEG),sample_EEG_filtered)
+
+
+
+# need a test for helper_functions.hash_it_up_right_all()
+
+
 if __name__ == '__main__':
     unittest.main()
+
