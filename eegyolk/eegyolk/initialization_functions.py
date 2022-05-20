e4a061578f10bb1397852eea0fde4b4c9a2bf3ce
FlorisP <Florispauwels@live.nl>
2022-05-20 10:39:26 +0200

    Change name: event_markers to events
    Change name: event_markers to events

--- eegyolk/eegyolk/initialization_functions.py
+++ eegyolk/eegyolk/initialization_functions.py
@@ -55,59 +55,59 @@ def load_metadata(folder, filenames):
     return metadata
 
 
-def save_event_markers(folder_event_markers, eeg_dataset, eeg_filenames):
+def save_events(folder_events, eeg_dataset, eeg_filenames):
     '''    
     Events are loaded from raw EEG files and saved in .txt file.
     Loading from .txt file is much faster than from EEG file.
     '''
-    if not(os.path.exists(folder_event_markers)):
-        os.mkdir(folder_event_markers)
+    if not(os.path.exists(folder_events)):
+        os.mkdir(folder_events)
 
     for i in range(len(eeg_dataset)):
-        event_marker_path = os.path.join(folder_event_markers, eeg_filenames[i] + ".txt")
-        np.savetxt(event_marker_path, mne.find_events(eeg_dataset[i]), fmt = '%i')
+        path_events = os.path.join(folder_events, eeg_filenames[i] + ".txt")
+        np.savetxt(path_events, mne.find_events(eeg_dataset[i]), fmt = '%i')
         print("\n", i+1, " out of ", len(eeg_dataset), " saved.")
         clear_output(wait=True)
 
-def caller_save_event_markers(folder_event_markers, generator_argument):
+def caller_save_events(folder_events, generator_argument):
     '''    
     Events are loaded from raw EEG files and saved in .txt file.
     Loading from .txt file is much faster than from EEG file.
     '''
-    if not os.path.isdir(folder_event_markers):
-        os.mkdir(folder_event_markers)
+    if not os.path.isdir(folder_events):
+        os.mkdir(folder_events)
 
     for i, (file, filename) in enumerate(generator_argument):
-        event_marker_path = os.path.join(folder_event_markers, filename + ".txt")
-        np.savetxt(event_marker_path, mne.find_events(file), fmt = '%i')
+        path_events = os.path.join(folder_events, filename + ".txt")
+        np.savetxt(path_events, mne.find_events(file), fmt = '%i')
         print("\n", i, " saved.")
         clear_output(wait=True)
 
-def load_event_markers(folder_event_markers, eeg_filenames):
+def load_events(folder_events, eeg_filenames):
     '''
     Events are saved and loaded externally from .txt file, 
     since loading events from raw EEG file takes much longer. 
     NB: eeg_filenames should not include extension or root directory.
     '''
-    if not(os.path.exists(folder_event_markers)):
-        print("There is no folder at: ", folder_event_markers,
+    if not(os.path.exists(folder_events)):
+        print("There is no folder at: ", folder_events,
               "\n first save the events in this folder.")
         return None
 
-    event_markers = []
+    events = []
     for filename in eeg_filenames:
-        filepath = os.path.join(folder_event_markers, filename + ".txt")
-        event_markers.append(np.loadtxt(filepath, dtype = int))    
-    print(len(event_markers), "Event Marker files loaded")
-    return event_markers
+        filepath = os.path.join(folder_events, filename + ".txt")
+        events.append(np.loadtxt(filepath, dtype = int))    
+    print(len(events), "Event Marker files loaded")
+    return events
 
 
-def print_event_info(event_markers, participant_index = 5, event_index = 500, sample_frequency = 2048):
+def print_event_info(events, participant_index = 5, event_index = 500, sample_frequency = 2048):
     '''
     Prints information on a specified event marker.
     '''
-    event_time = event_markers[participant_index][event_index][0]
-    event_ID = event_markers[participant_index][event_index][2]
+    event_time = events[participant_index][event_index][0]
+    event_ID = events[participant_index][event_index][2]
     print((
         "Participant {} heard event ID: {} after {:.1f} seconds "
         "as the {} event"
