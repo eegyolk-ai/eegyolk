48fd624687523b72ae43f846417ed903cfb4f374
Floris <>
2022-09-07 16:32:28 +0000

    work in progress

--- floris_files/models/dnn.py
+++ floris_files/models/dnn.py
@@ -48,7 +48,7 @@ def fully_connected_model():
     """ Returns the fully connected model from Ismail Fawaz et al. (2019). """
     n_timesteps = 512
     n_features = 32
-    n_outputs = 5
+    n_outputs = 3
 
     input_shape = (n_timesteps, n_features)
 
@@ -57,13 +57,13 @@ def fully_connected_model():
     input_layer_flattened = keras.layers.Flatten()(input_layer)
 
     layer_1 = keras.layers.Dropout(0.5)(input_layer_flattened)
-    layer_1 = keras.layers.Dense(2000, activation='relu')(layer_1)
+    layer_1 = keras.layers.Dense(100, activation='relu')(layer_1)
 
     layer_2 = keras.layers.Dropout(0.4)(layer_1)
-    layer_2 = keras.layers.Dense(500, activation='relu')(layer_2)
+    layer_2 = keras.layers.Dense(50, activation='relu')(layer_2)
 
     layer_3 = keras.layers.Dropout(0.3)(layer_2)
-    layer_3 = keras.layers.Dense(100, activation='relu')(layer_3)
+    layer_3 = keras.layers.Dense(30, activation='relu')(layer_3)
 
     output_layer = keras.layers.Dropout(0.2)(layer_3)
     output_layer = keras.layers.Dense(n_outputs, activation='sigmoid')(output_layer)
